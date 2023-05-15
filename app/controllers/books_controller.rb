507cea10708d1a15f933606b3e4dda9b979a002b
EC2 Default User <ec2-user@ip-172-31-31-123.ap-northeast-1.compute.internal>
2023-05-15 03:49:04 +0000

    [finish]フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,12 +4,10 @@ class BooksController < ApplicationController
    end
 
   def create
-    @book = Book.new(book_params)
-    if @book.save
-     redirect_to book_path(@book.id)
-    else
-      render :new
-    end
+    book = Book.new(book_params)
+    book.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(book.id)
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'books/new'
+  root to: 'homes#top'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'top' => 'homes#top'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_list'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_list'
 end
 
 
