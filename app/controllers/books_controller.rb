49136f7c760cfc6cd88f630c7f8d939f77789a6d
EC2 Default User <ec2-user@ip-172-31-31-123.ap-northeast-1.compute.internal>
2023-05-15 14:34:03 +0000

    [install]Rspec

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,20 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-   end
+
 
   def create
-    book = Book.new(book_params)
-    book.save
+    @book = Book.new(book_params)
+    if @book.save
     flash[:notice] = "Book was successfully created."
-    redirect_to book_path(book.id)
+    redirect_to books_path(@book.id)
+  else
+    @books = Book.all
+    render :index
+  end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
