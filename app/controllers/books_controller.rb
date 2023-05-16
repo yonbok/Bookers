516aef3d4bab6a666df5b9fd0e318ca85b7b14e6
EC2 Default User <ec2-user@ip-172-31-31-123.ap-northeast-1.compute.internal>
2023-05-16 08:18:42 +0000

    [add]エラーメッセージ設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
     flash[:notice] = "Book was successfully created."
-    redirect_to books_path(@book.id)
+    redirect_to book_path(@book.id)
   else
     @books = Book.all
     render :index
@@ -26,9 +26,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book.id)
+  else
+    render :edit
+   end
   end
 
   def destroy
